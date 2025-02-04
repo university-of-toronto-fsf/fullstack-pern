2631677b2029e515b746e680167116a3040b9a9e
Edward Apostol <ed@edapostol.com>
2025-02-04 10:54:05 -0500

    WIP: additional refactoring of API and AUTH endpoints

--- server/src/routes/api/index.ts
+++ server/src/routes/api/index.ts
@@ -3,10 +3,10 @@
 // 3(a). import the express Router
 import { Router } from 'express';
 // 3(b). import the routes file used to process user related requests
-import userRoutes from './user-routes.js'; // note here we refer to the transpiled file
+import { userRouter } from './user-routes.js'; // note here we refer to the transpiled file
 
 // 3(c). create a new Router Object
 const apiRouter = Router();
-apiRouter.use('/users', userRoutes);
+apiRouter.use('/users', userRouter);
 
 export default apiRouter;
--- server/src/routes/auth/index.ts
+++ server/src/routes/auth/index.ts
@@ -3,10 +3,10 @@
 // 3(a). import the express Router
 import { Router } from 'express';
 // 3(b). import the routes file used to process user related requests
-import authRoutes from './auth-routes.js'; // note here we refer to the transpiled file
+import { authRouter } from './auth-routes.js'; // note here we refer to the transpiled file
 
 // 3(c). create a new Router Object
 const apiRouter = Router();
-apiRouter.use('/users', authRoutes);
+apiRouter.use('/users', authRouter);
 
 export default apiRouter;
