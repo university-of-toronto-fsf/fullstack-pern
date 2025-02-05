3d5b3424c875d059e5863db79bcbfe3431687abc
Edward Apostol <ed@edapostol.com>
2025-02-05 11:09:24 -0500

    fix: removed the /login route from the user api related routes (didn't make sense)

--- server/src/routes/api/user-routes.ts
+++ server/src/routes/api/user-routes.ts
@@ -26,46 +26,13 @@ router.get('/', async (req: Request, res: Response): Promise<void> => {
   res.status(200).json({ message: 'Hello user endpoint' });
 });
 
-router.post('/login', async (req: Request, res: Response) => {
-  console.log('Login endpoint');
-  console.log('req.url', req.url);
-  console.log('req.body', req.body);
-  /* 
-    // insert code here to check if there is a user with the provided credentials
-    // if the user exists, generate a JWT token and return it in the response
-    // if the user does not exist, return an error message or return user to login screen
-    // request.body should contain the user's credentials (username and password)
-    // use the compare() method from bcrypt to compare the provided password 
-    // with the hashed password
-  */
-
-  // for now, we will create a pretend user and return a pretend token for testing
-  const pretendUser = {
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-  };
-
-  // Get the secret key from environment variables
-  const secretKey = process.env.JWT_SECRET_KEY || '';
-
-  // Generate a JWT token for the authenticated user
-  const token = jwt.sign({ username: pretendUser.username }, secretKey, {
-    expiresIn: '1h',
-  });
-
-  res.json({
-    description: 'Login Endpoint',
-    token: token,
-  });
-});
-
 // example test endpoint /api/users/getUsers
 const getUsers = async (req: Request, res: Response): Promise<void> => {
   console.log('Get Users endpoint');
   console.log('req.url', req.url);
   console.log('req.body', req.body);
 
+  /* 
   try {
     const authHeader = req.headers['authorization'];
     if (!authHeader) {
@@ -103,6 +70,11 @@ const getUsers = async (req: Request, res: Response): Promise<void> => {
     console.error('Error in getUsers endpoint:', error);
     res.status(500).json({ message: 'Internal server error' });
   }
+
+*/
+  // Once the token is validated, access the DB to return a known list of users
+  // for now we will return a pretend list of users
+  res.status(200).json({ users: ['Alice', 'Bob', 'Charlie'] });
 };
 
 router.get('/getUsers', getUsers);
