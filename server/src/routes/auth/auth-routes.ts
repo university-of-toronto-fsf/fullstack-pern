bcc3a2a6ea4f76eae936e01ff3bb735b7b81e06c
Edward Apostol <ed@edapostol.com>
2025-02-05 11:08:24 -0500

    fix: moved /login route to the auth set of routes

--- server/src/routes/auth/auth-routes.ts
+++ server/src/routes/auth/auth-routes.ts
@@ -15,12 +15,49 @@ import express from 'express';
 // 5(a). import Request and Response types from express
 import { type Request, type Response } from 'express';
 
+// import jsonwebtoken
+import jwt from 'jsonwebtoken';
+
 const router = express.Router();
 
 router.get('/', (req: Request, res: Response) => {
   console.log('Hello auth endpoint!');
   console.log('req.url', req.url);
   res.json({ message: 'Hello auth endpoint' });
+});
+
+router.post('/login', async (req: Request, res: Response) => {
+  console.log('Login endpoint');
+  console.log('req.url', req.url);
+  console.log('req.body', req.body);
+  /*
+    // insert code here to check if there is a user with the provided credentials
+    // if the user exists, generate a JWT token and return it in the response
+    // if the user does not exist, return an error message or return user to login screen
+    // request.body should contain the user's credentials (username and password)
+    // use the compare() method from bcrypt to compare the provided password
+    // with the hashed password
+  */
+
+  // for now, we will create a pretend user and return a pretend token for testing
+  const pretendUser = {
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  };
+
+  // Get the secret key from environment variables
+  const secretKey = process.env.JWT_SECRET_KEY || '';
+
+  // Generate a JWT token for the authenticated user
+  const token = jwt.sign({ username: pretendUser.username }, secretKey, {
+    expiresIn: '1h',
+  });
+
+  res.json({
+    description: 'Login Endpoint',
+    token: token,
+  });
 });
 
 export { router as authRouter };
