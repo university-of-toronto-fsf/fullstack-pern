d6564b0886255b1f7e322c8a7c43fd378ee88067
Edward Apostol <ed@edapostol.com>
2025-02-05 11:09:03 -0500

    feat: added express middleware function to authenticate every single time

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,12 +13,22 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // 1(e). Define the port number
+// import the middleware function
+import verifyJWT from './routes/middleware/verify-jwt.js';
+
+// 1(f). Define the port number
 console.log(`process.env.PORT: ${process.env.PORT}`);
 const PORT = process.env.PORT || 3002;
 
-// 1(f). add JSON parsing middleware before the routes are set up
+// 1(g). add JSON parsing middleware before the routes are set up
 APP.use(express.json());
 APP.use(express.urlencoded({ extended: true }));
+
+// 1(h). before we define the routes, we need to add the middleware functions
+// add the middleware function to the express app
+// in this case, the middleware function is verifyJWT which will be used to
+// verify the JWT token as this has to happen upon every request to the server
+APP.use(verifyJWT);
 
 // 1(g). Define directory for static files and connect routes to express app
 APP.use(express.static('../client/dist'));
