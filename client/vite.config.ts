af03f160f6b7f91985d202d90a3fc000233cee04
Edward Apostol <ed@edapostol.com>
2025-02-06 10:41:00 -0500

    feat: configured vite config to switch between development and render prod settings

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,23 +3,28 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/' /* for render deployment */,
   server: {
     port: 3000,
     open: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3002' /* note to change in production */,
-        changeOrigin: true,
-        secure: false /* to adjust in production for https:// to true */,
-        /* rewrite: (path) => path.replace(/^\/api/, ''), */
-      },
-      '/auth': {
-        target: 'http://localhost:3002',
-        changeOrigin: true,
-        secure: false /* to adjust in production for https:// to true */,
-        /* rewrite: (path) => path.replace(/^\/api/, ''), */
-      },
-    },
+    proxy:
+      process.env.NODE_ENV === 'developmeent'
+        ? {
+            '/api': {
+              target:
+                'http://localhost:3002' /* note to change in production */,
+              changeOrigin: true,
+              secure: false /* to adjust in production for https:// to true */,
+              /* rewrite: (path) => path.replace(/^\/api/, ''), */
+            },
+            '/auth': {
+              target: 'http://localhost:3002',
+              changeOrigin: true,
+              secure: false /* to adjust in production for https:// to true */,
+              /* rewrite: (path) => path.replace(/^\/api/, ''), */
+            },
+          }
+        : undefined, // diable proxy in production
   },
   plugins: [react()],
 });
