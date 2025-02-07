4631cda2798cffd601d4387b1b2ee19d47e14c93
Edward Apostol <ed@edapostol.com>
2025-02-06 20:14:46 -0500

    WIP: continuing changes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   base: '/' /* for render deployment */,
+  plugins: [react()],
   server: {
     port: 3000,
     open: true,
@@ -26,5 +27,4 @@ export default defineConfig({
           }
         : undefined, // diable proxy in production
   },
-  plugins: [react()],
 });
