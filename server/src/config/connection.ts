fc74e3757ed125e8eda082904b12d53d2b25e162
Edward Apostol <ed@edapostol.com>
2025-02-08 13:45:50 -0500

    fix: updated local environment vars with LOCAL prefix and added SSL for prod

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -3,14 +3,39 @@ dotenv.config();
 
 import { Sequelize } from 'sequelize';
 
+/*
+  // make sure you have the .env file with the DB_URL
+  // variable filled in with the connection string
+  // provided by render.com when you create a new database
+  // and add the setting to the environment variables
+  // in the render.com dashboard
+*/
 const isUsingRemoteDB = Boolean(process.env.DB_URL);
 
+/*
+  // as discussed in class, I substituted the original code
+  // that checks for the presence of the DB_URL variable
+  // with a variable that is set to true or false based on
+  // the presence of the DB_URL variable
+*/
+
 const sequelize = isUsingRemoteDB
-  ? new Sequelize(process.env.DB_URL as string)
+  ? new Sequelize(process.env.DB_URL as string, {
+      dialect: 'postgres',
+      dialectOptions: {
+        decimalNumbers: true,
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        } /* 
+          here, we are telling the database to accept all SSL certificates
+          regardless of their origin (for example from render.com) */,
+      },
+    })
   : new Sequelize(
-      process.env.DB_NAME || '',
-      process.env.DB_USER || '',
-      process.env.DB_PASSWORD,
+      process.env.LOCAL_DB_NAME || '',
+      process.env.LOCAL_DB_USER || '',
+      process.env.LOCAL_DB_PASSWORD,
       {
         host: 'localhost',
         dialect: 'postgres',
