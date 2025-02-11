eee3d84c0e0b2b1225c629e9d469c0963ed49bfa
Edward Apostol <ed@edapostol.com>
2025-02-11 14:30:34 -0500

    feat: created the new bootstrap navbar component - https://react-bootstrap.github.io/docs/components/navbar

---
+++ client/src/components/navbar/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import { Person, Gear, BoxArrowRight } from 'react-bootstrap-icons';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+/*
+ * Navbar component
+ * Navbar component contains the navigation bar of the application
+ * It contains the logo, search bar, and the user profile, settings, and logout buttons
+ */
+
+/*
+ * Nav.Link as={Link} to="/profile"
+ * This is a react-bootstrap component that is used to
+ * create a link in the navigation bar
+ * Nav is a react-bootstrap component
+ * .Link is a sub-component of Nav used to create links
+ * https://react-bootstrap.github.io/docs/components/navbar
+ */
+const NavbarComponent: React.FC = () => {
+  return (
+    <Navbar bg="light" expand="lg" className="mb-4">
+      <Navbar.Brand href="#home">Logo</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Form className="form-inline d-flex">
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">
+              <i className="bi bi-search"></i>
+            </Button>
+          </Form>
+        </Nav>
+        <Nav>
+          <Nav.Link as={Link} to="/profile">
+            <Person size={20} />
+          </Nav.Link>
+          <Nav.Link as={Link} to="/settings">
+            <Gear size={20} />
+          </Nav.Link>
+          <Nav.Link as={Link} to="/logout">
+            <BoxArrowRight size={20} />
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default NavbarComponent;
