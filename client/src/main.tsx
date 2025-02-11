7a94018dbd7f88f7b1688fc5f7de17b5c0bbca63
Edward Apostol <ed@edapostol.com>
2025-02-11 16:55:34 -0500

    fix: replaced empty browserrouter with routerprovider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,10 +2,31 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 // import { ReactDOM } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import {
+  /* BrowserRouter, */
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
 
 import App from './App.tsx';
+import HomePageComponent from './pages/home/index.tsx';
+import LoginPageComponent from './pages/login/index.tsx';
+
 import './index.css';
+
+const routes = [
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { index: true, element: <HomePageComponent /> },
+      { path: '/login', element: <LoginPageComponent /> },
+      {},
+    ],
+    errorElement: <div>404 Not Found (Component to be Created)</div>,
+  },
+];
+const router = createBrowserRouter(routes);
 
 // Explanation:
 // react-router-dom is used for routing in React applications.
@@ -15,8 +36,6 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>
 );
