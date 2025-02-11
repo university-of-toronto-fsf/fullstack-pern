1b90bc2a4a4a31d456df04afde7d9a9bbc068d52
Edward Apostol <ed@edapostol.com>
2025-02-10 22:58:06 -0500

    feat: (test) create example call to back-end to pull data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,37 @@ import './App.css';
 function App() {
   const [count, setCount] = useState(0);
 
+  async function handleButtonClick() {
+    let token = localStorage.getItem('ID_TOKEN');
+    // console.log('token', token);
+
+    if (!token) {
+      const loginResponse = await fetch('/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: '',
+          password: '',
+        }),
+      });
+      const loginData = await loginResponse.json();
+      token = loginData.token;
+      if (token) {
+        localStorage.setItem('ID_TOKEN', token);
+      }
+    }
+
+    const usersResponse = await fetch('/api/users/getUsers', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const usersData = await usersResponse.json();
+    console.log(usersData);
+  }
+
   return (
     <>
       <div>
@@ -19,9 +50,10 @@ function App() {
           className="btn"
           type="button"
           onClick={async () => {
-            const res = await fetch('/api/test');
-            const data = await res.json();
-            console.log(data);
+            // const res = await fetch('/api/test');
+            // const data = await res.json();
+            // console.log(data);
+            handleButtonClick();
           }}
         >
           click me to test DB
